b2677a3b314e556b042f5f8e097dee6a703c44b4
krists0 <christina0147@gmail.com>
2022-11-09 15:14:34 +0200

    commit#3

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 console.log("script");
 const img = document.getElementById('carousel');
+const imgClass=document.getElementById('carousel').className;
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
 
-
-
-let pictures = ['https://images.unsplash.com/photo-1537000092872-06bbf7b64f60?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14d2fe1244b43a1841569da918066fc4&auto=format&fit=crop&w=1050&q=80',
+console.log("imgaes.className="+imgClass);
+let pictures = [];
+if(imgClass=="earrings"){
+     pictures = [
+ 'https://i.ibb.co/0K8yvyp/silver-r2.jpg',
+'https://i.ibb.co/CK23GtZ/Whats-App-Image-2022-10-05-at-00-14-23-1.jpg'
+];
+}
+else{
+    pictures = ['https://images.unsplash.com/photo-1537000092872-06bbf7b64f60?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14d2fe1244b43a1841569da918066fc4&auto=format&fit=crop&w=1050&q=80',
  'https://i.ibb.co/0K8yvyp/silver-r2.jpg',
   'https://i.ibb.co/8KscsVr/silver-ring3.jpg',
 'https://i.ibb.co/CK23GtZ/Whats-App-Image-2022-10-05-at-00-14-23-1.jpg'
 ];
+}
+
 
 img.src = pictures[0];
 let position = 0;
